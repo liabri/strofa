f72d3fff44aaa39422f5dbe12840b49fc9abe0b0
lb06 <lbcat@tutanota.com>
2022-02-05 11:22:19 +0100

    cleanup events

--- src/event.rs
+++ src/event.rs
@@ -1,25 +1,10 @@
-use std::{ sync::mpsc, thread, time::Duration };
 use crossterm::event;
 
-#[derive(Debug, Clone, Copy)]
-pub struct EventConfig {
-    pub tick_rate: Duration,
-}
-
-impl Default for EventConfig {
-    fn default() -> EventConfig {
-        EventConfig {
-            tick_rate: Duration::from_millis(250),
-        }
-    }
-}
-
+pub struct Events;
 pub enum Event {
     Input(Key),
     Tick
 }
-
-pub struct Events;
 
 use futures_util::Stream;
 use async_stream::stream;
@@ -38,11 +23,6 @@ impl Events {
         }
     }
 }
-
-// pub struct Events {
-//     rx: mpsc::Receiver<Event<Key>>,
-//     _tx: mpsc::Sender<Event<Key>>
-// }
 
 #[derive(PartialEq, Eq, Clone, Copy, Hash, Debug)]
 pub enum Key {
@@ -96,43 +76,3 @@ impl From<event::KeyEvent> for Key {
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// impl Events {
-//     pub fn new() -> Events {
-//         Events::with_config(EventConfig::default())
-//     }
-
-//     pub fn with_config(config: EventConfig) -> Events {
-//         let (tx, rx) = mpsc::channel();
-
-//         let event_tx = tx.clone();
-//         thread::spawn(move || {
-//             loop {
-//                 if event::poll(config.tick_rate).unwrap() {
-//                     if let event::Event::Key(key) = event::read().unwrap() {
-//                         let key = Key::from(key);
-//                         event_tx.send(Event::Input(key)).unwrap();
-//                     }
-//                 }
-
-//                 event_tx.send(Event::Tick).unwrap();
-//             }
-//         });
-
-//         Events { rx, _tx: tx }
-//     }
-
-//     pub fn next(&self) -> Result<Event<Key>, mpsc::RecvError> { self.rx.recv() }
-// }
