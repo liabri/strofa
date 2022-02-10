8e15507f80b0e91ad621a29606666f03ab386a2c
lb06 <lbcat@tutanota.com>
2022-02-10 23:50:47 +0100

    move general key event methods to 'Block' module

--- src/block/mod.rs
+++ src/block/mod.rs
@@ -129,6 +129,92 @@ impl<B: 'static + Send + Backend> Blocks<B> {
         self.hover_history.push_front(self.hovered.clone());
         self.hovered = blk.clone();
     } 
+
+    pub async fn active_event(state: &mut State<B>, key: Key) {
+        match state.blocks.active {
+            // Some(BlockKind::Search) => Search::active_key_event(self, key).await,
+            // Some(BlockKind::Sort) => Sort::active_key_event(self, key).await,
+            Some(BlockKind::Library) => IndexedBlock::<Library>::active_event(state, key).await,
+            Some(BlockKind::Playlists) => IndexedBlock::<Playlists>::active_event(state, key).await,
+
+        //     Some(BlockKind::Main) => { 
+        //         match key {
+        //             Key::Up => self.blocks.main.index().dec(),
+        //             Key::Down => self.blocks.main.index().inc(),
+        //             _ => {}
+        //         }
+
+        //         match &self.blocks.main {
+        //             MainBlock::Tracks(x) => {
+        //                 match key {
+        //                     Key::Enter => x.play(&self.client, x.index.inner).await,
+        //                     // Key::Char('A') => self.client.add_song_to_playlist(x.songs.get(x.index.inner).unwrap()).await
+        //                     _ => {}
+        //                 }
+        //             },
+
+        //             MainBlock::Queue(x) => {
+        //                 // x.active_key_event(self, key);
+        //                 // match key {
+        //                 //     Key::Enter => x.play(self.client.clone(), x.index.inner).await,
+        //                 //     Key::Char('c') => self.client.clear_queue().await.unwrap(),
+        //                 //     // Key::Char('p') => self.client.proritise_song_in_queue(x.index.inner)
+        //                 //     // Key::Char('w') => self.client.move_song_up_in_queue(x.songs.get(x.index.inner).unwrap()).await
+        //                 //     // Key::Char('s') => self.client.move_song_down_in_queue(x.songs.get(x.index.inner).unwrap()).await
+        //                 //     // Key::Char('A') => self.client.add_song_to_playlist(x.songs.get(x.index.inner).unwrap()).await
+        //                 //     // Key::Char('o') => x.jump_to_current_song().await
+        //                 //     _ => {}
+        //                 // }
+        //             },
+
+        //             _ => {}
+        //         }
+        //     },
+
+            _ => {}
+        }
+    }
+
+    pub async fn hovered_event(state: &mut State<B>, key: Key) {
+        match state.blocks.hovered {
+            // BlockKind::Search => Search::hovered_key_event(self, key).await,
+            // BlockKind::Sort => Sort::hovered_key_event(self, key).await,
+            BlockKind::Library => IndexedBlock::<Library>::hovered_event(state, key).await,
+            BlockKind::Playlists => IndexedBlock::<Playlists>::hovered_event(state, key).await,
+
+            // BlockKind::Main => {
+            //     match key {
+            //         Key::Up => self.blocks.set_hover(&BlockKind::Search),
+            //         Key::Left => {
+            //             for previous in self.blocks.hover_history.clone().into_iter() {
+            //                 if previous==BlockKind::Library || previous==BlockKind::Playlists {
+            //                     self.blocks.set_hover(&previous);
+            //                     return;
+            //                 }
+            //             }
+
+            //             self.blocks.set_hover(&BlockKind::Library)
+            //         },
+
+            //         Key::Right => self.blocks.set_hover(&BlockKind::Sort),
+            //         Key::Down => {
+            //             self.blocks.set_active(BlockKind::Main);
+            //             self.blocks.main.index().inc();
+            //         },
+
+            //         _ => {},
+            //     }
+            // },
+
+            _ => {}   
+        }
+
+        // common behaviour
+        match key {
+            Key::Enter => state.blocks.set_active(state.blocks.hovered),
+            _ => {}
+        }
+    }   
 }
 
 #[async_trait]
